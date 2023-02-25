3013323730dff0e808269dc78849f29ca408085d
YangBiao <1348719184@qq.com>
2023-02-25 22:47:44 +0800

    start

---
+++ yiaobang-blog/src/main/java/com/y/YiaoBangBlogApplication.java
@@ -0,0 +1,17 @@
+package com.y;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 博客应用程序
+ *
+ * @author Y
+ * @date 2023/02/25
+ */
+@SpringBootApplication
+public class YiaoBangBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YiaoBangBlogApplication.class,args);
+    }
+}
